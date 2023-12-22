52ee04ae09beae88a038e95ed545b5c4e79eb9e2
Manohar-1 <mudhapakamanohar@gmail.com>
2023-12-23 00:46:51 +0530

    section hands on with angular completed

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 
 
@@ -18,15 +19,14 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials';
   invalidLogin = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public hardcodedAuthenticationService:HardcodedAuthenticationService) {}
 
   ngOnInit() {}
 
   handleLogin() {
-    if (this.username === 'manohar' && this.password === 'manohar123') {
+    // if (this.username === 'manohar' && this.password === 'manohar123') {
+    if (this.hardcodedAuthenticationService.authenticate(this.username,this.password)) {
       this.invalidLogin = false;
-
-      //Redirect to Welcome Page
       this.router.navigate(['welcome',this.username]);
     } else {
       this.invalidLogin = true;
--- frontend/todo/src/app/menu/menu.component.ts
+++ frontend/todo/src/app/menu/menu.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+import { RouterModule } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule,FormsModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  // userLoggedIn = false;
+
+  constructor(public hardcodedAuthenticationService:HardcodedAuthenticationService){
+
+  } 
+  ngOnInit(): void {
+    // this.userLoggedIn = this.hardcodedAuthenticationService.isUserLoggedIn(); 
+    // console.log(this.userLoggedIn);
+  }
+
 
 }
